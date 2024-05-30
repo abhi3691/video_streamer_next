a81ba4e61375b219de0e659a2d2b29ee447af498
abhinand <abhinandvk41@wefttechnologies.com>
2024-05-30 22:21:25 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,37 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import axios from "axios";
+import ReactPlayer from "react-player";
 
 export default function AdminPanel() {
   const [uploadedVideos, setUploadedVideos] = useState<any[]>([]);
-  const [selectedVideo, setSelectedVideo] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [toastMessage, setToastMessage] = useState("");
+  const [selectedVideo, setSelectedVideo] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [toastMessage, setToastMessage] = useState<string>("");
 
-  const handleFormSubmit = async (event: any) => {
+  useLayoutEffect(() => {
+    const loadVideos = async () => {
+      try {
+        const response = await axios.get("/api/videos");
+        setUploadedVideos(response?.data?.urls);
+        if (response?.data?.urls?.length) {
+          setSelectedVideo(response?.data?.urls[0].url);
+        }
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    };
+    loadVideos();
+  }, []);
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setLoading(true);
     setToastMessage("");
     const formData = new FormData();
-    const fileField: any = document.getElementById("videoFile");
+    const fileField: any = document.getElementById(
+      "videoFile"
+    ) as HTMLInputElement;
 
     formData.append("video", fileField.files[0]);
 
@@ -21,33 +39,13 @@ export default function AdminPanel() {
       const response = await axios.post("/api/upload", formData);
       setLoading(false);
       setToastMessage("Video uploaded successfully!");
-      loadVideos(false);
-      playVideo(response.data.url); // Automatically play the uploaded video
+      setUploadedVideos([...uploadedVideos, response.data.url]);
+      setSelectedVideo(response.data.url);
     } catch (error) {
       setLoading(false);
       setToastMessage("Error uploading video.");
       console.error("Error uploading video:", error);
     }
-  };
-
-  const loadVideos = useCallback(async (isInitial: boolean) => {
-    try {
-      const response: any = await axios.get("/api/videos");
-      setUploadedVideos(response?.data?.urls);
-      if (isInitial && response?.data?.urls?.length) {
-        playVideo(response?.data?.urls[0].url);
-      }
-    } catch (error) {
-      console.error("Error fetching videos:", error);
-    }
-  }, []);
-
-  useEffect(() => {
-    loadVideos(true);
-  }, [loadVideos]);
-
-  const playVideo = (videoUrl: any) => {
-    setSelectedVideo(videoUrl);
   };
 
   return (
@@ -90,13 +88,13 @@ export default function AdminPanel() {
           </h2>
           <select
             value={selectedVideo}
-            onChange={(e) => playVideo(e.target.value)}
+            onChange={(e) => setSelectedVideo(e.target.value)}
             className="w-full bg-gray-700 text-white p-3 border border-gray-600 rounded-md focus:outline-none"
           >
             <option value="">Select a video</option>
-            {uploadedVideos.map((videoUrl, index) => (
-              <option key={index} value={videoUrl?.url}>
-                {videoUrl?.url}
+            {uploadedVideos.map((value, index) => (
+              <option key={index} value={value?.url}>
+                {value?.url}
               </option>
             ))}
           </select>
@@ -104,13 +102,13 @@ export default function AdminPanel() {
 
         <div className="bg-gradient-to-br from-gray-800 via-gray-700 to-gray-900 rounded-lg shadow-lg mb-8 overflow-hidden">
           {selectedVideo && (
-            <video
-              className="w-full"
+            <ReactPlayer
+              url={selectedVideo}
               controls
-              autoPlay
-              muted
-              src={selectedVideo}
-            ></video>
+              playing={selectedVideo !== ""}
+              width="100%"
+              height="100%"
+            />
           )}
         </div>
       </div>
